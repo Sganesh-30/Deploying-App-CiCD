e269311316cb761cb52d32dbe3453427f1c9eafc
Ganesh <ganeshsg430@gmail.com>
2025-01-24 14:16:54 +0530

    Added all files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Game!');
+});
+
+app.get('/game', (req, res) => {
+  res.send('Game is in progress...');
+});
+
+app.listen(port, () => {
+  console.log(`Game app listening at http://localhost:${port}`);
+});
---
+++ test/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', function() {
+  it('responds with Welcome to the Game!', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /text/)
+      .expect(200, 'Welcome to the Game!', done);
+  });
+});
+
+describe('GET /game', function() {
+  it('responds with Game is in progress...', function(done) {
+    request(app)
+      .get('/game')
+      .expect('Content-Type', /text/)
+      .expect(200, 'Game is in progress...', done);
+  });
+});
