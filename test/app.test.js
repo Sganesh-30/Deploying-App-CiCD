57df295c8e0663fe8688cf2cef1d4ff3f5e54852
Ganesh <ganeshsg430@gmail.com>
2025-01-24 14:29:12 +0530

    modified test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../app'); // Import the app instance
 
 describe('GET /', function() {
   it('responds with Welcome to the Game!', function(done) {
